17864a97b83d944aafdec986f7adf1cd3ef17399
Stud. Pietro Gariuolo <p.gariuolo.0893@vallauri.net>
2020-10-02 13:04:29 +0200

    Angular first exercise

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  names = ["Barack Obama","Lewis Hamilton","Scarlett Johansson","Antonio Conte","Marcello Rossi","Alberto Barbero","Debora Servetti","Paolo Cortese","Paolino Paperino"];
+  myName = 'Pierto';
+
+  changeName(){
+    this.myName = this.names[this.getRandomInt(0,8)];
+  }
+
+   getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
